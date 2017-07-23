e6aceef8f4443fb11df1721c2b7b8abbcc04fd5c
fsoar <jnan88@qq.com>
2017-07-23 21:48:36 +0800

    Update seafile_install.sh

--- linux_ops/seafile_install.sh
+++ linux_ops/seafile_install.sh
@@ -1,1 +1,2 @@
 yum install python-setuptools python-imaging python-ldap MySQL-python python-memcached memcached-devel memcached -y
+http://seafile-downloads.oss-cn-shanghai.aliyuncs.com/seafile-server_6.1.1_x86-64.tar.gz
