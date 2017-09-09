eda5cc59c07ab5458fb40fdad86799e36241e6c0
fsoar <jnan88@qq.com>
2017-09-09 16:41:53 +0800

    Update percona-mysql-install.sh

--- linux_ops/percona-mysql-install.sh
+++ linux_ops/percona-mysql-install.sh
@@ -7,10 +7,18 @@ rpm -ivh *.rpm
 默认密码在：grep password /var/log/mysqld.log
 首次登录需呀修改一次
 set global validate_password_policy=0;
-ALTER USER 'root'@'localhost' IDENTIFIED BY 'dev@mysql';
+ALTER USER 'root'@'localhost' IDENTIFIED BY 'dev#mysql';
 flush privileges;
+# slave
+create user slave identified by 'mysql#slave123';
+grant replication slave,replication client on *.* to 'slave'@'192.168.1.%' identified by 'mysql#slave123';
+
+
+create database dev default charset utf8 collate utf8_general_ci;
+grant all on dev.* to dev
 
 
 create user dev identified by 'mysql@dev';
 create database dev default charset utf8 collate utf8_general_ci;
 grant all on dev.* to dev
+
