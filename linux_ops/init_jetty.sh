329dbb25cb9c2f3b7f5dcf63d2fa64f26289407a
fsoar <jnan88@qq.com>
2017-10-26 03:08:42 -0500

    Update init_jetty.sh

--- linux_ops/init_jetty.sh
+++ linux_ops/init_jetty.sh
@@ -34,7 +34,7 @@ cat > $JETTY_BASE/resources/log4j2.xml <<END
 
     <RollingFile name="file"
       fileName="logs/jetty.log"
-      filePattern="logs/jetty-%d{MM-dd-yyyy}.log.gz"
+      filePattern="logs/jetty.log.%d{yyyy-MM-dd}"
       ignoreExceptions="false">
       <PatternLayout>
         <Pattern>%d{yyyy-MM-dd HH:mm:ss} %-5p %c{1}:%L - %m%n</Pattern>
@@ -71,7 +71,7 @@ JETTY_STATE=${JETTY_BASE}/${NAME}.state
 -XX:MetaspaceSize=128m
 -XX:MaxMetaspaceSize=512m
 -XX:AutoBoxCacheMax=20000
--Xloggc:/www/logs/gc-www.log
+-Xloggc:/www/log/gc-www.log
 -XX:+HeapDumpOnOutOfMemoryError
 -XX:+PrintGCDetails
 -XX:+PrintGCDateStamps
@@ -80,8 +80,8 @@ JETTY_STATE=${JETTY_BASE}/${NAME}.state
 -XX:+PrintGCApplicationStoppedTime
 -XX:+PrintPromotionFailure
 -XX:+UseGCLogFileRotation
--XX:NumberOfGCLogFiles=10
--XX:GCLogFileSize=10M
+-XX:NumberOfGCLogFiles=120
+-XX:GCLogFileSize=500M
 -Dcom.sun.management.jmxremote
 -Djava.rmi.server.hostname=ip
 -Dcom.sun.management.jmxremote.port=50001
